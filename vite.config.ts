105aa46c73d69d10b4b430065cddb5072ad5777c
Daniel Long <dl@Daniels-MacBook-Air.local>
2026-07-26 10:40:00 +0800

    this is my initial submit

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,21 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+  server: {
+    proxy: {
+      // with options:
+      // http://localhost:5173/api/bar
+      //   -> http://jsonplaceholder.typicode.com/bar
+      '/api': {
+        target: 'http://localhost:6000',
+        // -> http://localhost:6000/bar
+        changeOrigin: true,
+      },
+    },
+  },
 });
+
+
+// miss Dutt if you are reading this,
+// im trying to build the frontend by using the app vites, 
+// it has a free code showcase so there is no plagiarism here :)
